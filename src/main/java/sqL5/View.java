447ecc0957b2b3912479819ae5d1c2d117281b28
xpulvi <pulvirenti.n.a@gmail.com>
2022-07-13 17:25:21 +0200

    sql5MinorChange1

--- src/main/java/sqL5/View.java
+++ src/main/java/sqL5/View.java
@@ -1,9 +1,6 @@
 package sqL5;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,36 +12,50 @@ public class View {
         String user = "developer";
         String password = "test";
         Statement statement = null;
-
+        ResultSet resultSet = null;
+        ResultSet resultSet2 = null;
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
 
 
              statement = conn.createStatement();
             //create query
-            String query = "CREATE VIEW viewItalian AS SELECT last_name, first_name FROM students WHERE country = 'Italy';";
+            String query = "CREATE VIEW italian_students AS SELECT last_name, first_name FROM students WHERE country = 'Italy';";
 
             statement.executeUpdate(query);
 
-            query = "CREATE VIEW viewGerman AS SELECT last_name, first_name FROM students WHERE country = 'Germany';";
+            query = "CREATE VIEW german_students AS SELECT last_name, first_name FROM students WHERE country = 'Germany';";
             statement.executeUpdate(query);
 
-            //select students italian
-            statement.executeUpdate("SELECT * FROM viewItalian");
+
+
             //list obj studentsIta
             ArrayList<Student> studentsIta = new ArrayList<>();
+            resultSet = statement.executeQuery("SELECT * FROM italian_students");
             //put result on ArrayList
-         //   studentsIta.add();
+            while(resultSet.next()){
+                String name = resultSet.getString("first_name");
+                String surname = resultSet.getString("last_name");
+                studentsIta.add(new Student(name, surname));
+            }
+
+
 
             //select students italian
-            statement.executeUpdate("SELECT * FROM viewGerman");
+            resultSet2 = statement.executeQuery("SELECT * FROM german_students");
             ArrayList<Student> studentsGer = new ArrayList<>();
-           // studentsGer.add();
+            while(resultSet2.next()){
+                String name = resultSet2.getString("first_name");
+                String surname = resultSet2.getString("last_name");
+                studentsGer.add(new Student(name, surname));
+            }
 
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
-            statement.close();
+            //statement.close();
+           // resultSet.close();
+          //  resultSet2.close();
         }
 
     }
